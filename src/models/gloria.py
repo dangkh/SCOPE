4df1220e4d08d2cc787de9874722c8bfe827b8a3
dangkh <dangkh109@gmail.com>
2026-05-14 14:41:17 +1000

    Update gloria.py

--- src/models/gloria.py
+++ src/models/gloria.py
@@ -129,7 +129,7 @@ class GLORIA(GeneralRecommender):
 
         self.out_proj = nn.Linear(3, 1) 
         self.userW = nn.Parameter(torch.empty(num_user, 1))
-        nn.init.xavier_uniform_(self.userW)
+        nn.init.xavier_normal_(self.userW)
 
         
 
@@ -177,7 +177,7 @@ class GLORIA(GeneralRecommender):
 
 
         item_feat = self.mlp_item(self.t_feat)
-        user_feat = F.normalize(self.mlp_user(self.user_feat))
+        user_feat = F.normalize(self.mlp_user(self.local_feat))
 
         self.t_rep, self.t_preference = self.t_gcn(self.edge_index, item_feat)
         self.idl_rep, self.idl_preference = self.idl_gcn(self.edge_index_low, self.id_embedding_low.weight)
@@ -197,12 +197,11 @@ class GLORIA(GeneralRecommender):
         # semantic Preference Conflict = 1 - cos(self.user_feat and self.local_feat)
         c_u = 1 - F.cosine_similarity(self.user_feat, self.local_feat, dim=-1)
         c_u = c_u.unsqueeze(-1)
-        gate_input =  user_reph - user_repl
-        # gate_input = F.layer_norm(user_reph - user_repl, [user_reph.size(-1)])
+        gate_input = F.layer_norm(user_reph - user_repl, [user_reph.size(-1)])
         gate_output = self.gate(gate_input)
         alpha_input = torch.cat([self.userW, c_u, gate_output], dim=1)
         alpha_e = torch.sigmoid(self.out_proj(alpha_input))  # [B, 1]
-        scale = 0.5 + 0.5 * alpha_e  # scale to [0.5, 1]
+        scale = 0.5 + 0.5 * alpha_e 
         user_reph = scale * user_reph
 
         user_rep = torch.cat((user_repT, user_repl, user_reph), dim=1)
@@ -268,9 +267,8 @@ class GCN(torch.nn.Module):
         x = F.normalize(x)
         h = self.conv_embed_1(x, edge_index)  # equation 1
         h_1 = self.conv_embed_1(h, edge_index)
-        h_2 = self.conv_embed_1(h_1, edge_index)
 
-        x_hat =h + x + h_1 + h_2
+        x_hat =h + x + h_1 
         return x_hat, self.preference
 
 
